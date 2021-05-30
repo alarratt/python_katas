621431af0850a00d513e3b4c9541fc8227edb363
alarratt <anthonylarratt@gmail.com>
2021-05-30 09:35:40 -0400

    Test

---
+++ Dubstep Kata.py
@@ -0,0 +1,10 @@
+song="WUBTHISWUBISWUBAWUBTESTWUB"
+#First, find any instances of multiple "WUB"s and replace with single "WUB"
+while 'WUBWUB' in song:
+    song = song.replace('WUBWUB', 'WUB')
+#Song should now only have single WUB's inside
+#Replace WUB with ' '
+song = song.replace('WUB', ' ')
+#Remove any spaces from the beginning and end of the string. 
+song = song.strip()
+print(song)
